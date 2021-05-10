37db50989785b8f0099175f81e406b656b33ca27
annaszyszkiewicz <250386@student.pwr.edu.pl>
2021-05-10 22:15:50 +0200

    Added token

---
+++ src/main/java/edu/ib/springdata/controller/LoginController.java
@@ -0,0 +1,39 @@
+package edu.ib.springdata.controller;
+
+import edu.ib.springdata.LoginUser;
+import edu.ib.springdata.service.UserDtoService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+    private UserDtoService userDtoService;
+
+    @Autowired
+    public LoginController(UserDtoService userDtoService) {
+        this.userDtoService = userDtoService;
+    }
+
+    @PostMapping("/getToken")
+    public String login(@RequestBody LoginUser loginUser) throws Exception {
+        String role = userDtoService.findUser(loginUser.getName(), loginUser.getPassword());
+
+            long currentTimeMillis = System.currentTimeMillis();
+            return Jwts.builder()
+                    .setSubject(loginUser.getName())
+                    .claim("roles", role)
+                    .setIssuedAt(new Date(currentTimeMillis))
+                    .setExpiration(new Date(currentTimeMillis + 60000))
+                    .signWith(SignatureAlgorithm.HS512, loginUser.getPassword())
+                    .compact();
+    }
+}
--- src/main/java/edu/ib/springdata/service/UserDtoService.java
+++ src/main/java/edu/ib/springdata/service/UserDtoService.java
@@ -1,8 +1,13 @@
 package edu.ib.springdata.service;
 
+import edu.ib.springdata.PasswordEncoderConfig;
 import edu.ib.springdata.repository.UserDtoRepository;
+import edu.ib.springdata.repository.entity.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
 
 @Service
 public class UserDtoService {
@@ -10,6 +15,23 @@ public class UserDtoService {
     private UserDtoRepository userDtoRepository;
 
     @Autowired
-    public UserDtoService(UserDtoRepository userDtoRepository) {this.userDtoRepository = userDtoRepository;}
+    public UserDtoService(UserDtoRepository userDtoRepository) {
+        this.userDtoRepository = userDtoRepository;
+    }
 
+    public String findUser(String login, String pass) throws Exception {
+        PasswordEncoderConfig passwordEncoderConfig = new PasswordEncoderConfig();
+        PasswordEncoder passwordEncoder = passwordEncoderConfig.passwordEncoder();
+        Iterable<UserDto> iterable = userDtoRepository.findAll();
+        Iterator<UserDto> iterator = iterable.iterator();
+        String string = null;
+        while (iterator.hasNext()) {
+            UserDto userDto = iterator.next();
+            if (userDto.getName().equals(login) && passwordEncoder.matches(pass, userDto.getPasswordHash()))
+                string = userDto.getRole();
+        }
+        if (string == null)
+            throw new Exception("Wrong login or password");
+        return string;
+    }
 }
